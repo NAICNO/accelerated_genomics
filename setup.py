5ad7272e8ea600c430e7514ca19f9e41241e8a39
Pubudu Saneth Samarakoon <saneth.samarakoon@gmail.com>
2023-09-07 15:38:19 +0200

    Setting up the structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing and testing NVDIA's Clara Parabricks is the software suite developed to analyze NGS data GPUs',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
