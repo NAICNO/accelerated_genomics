fdb0d802d1edee52b2fb302027a746c24d29b609
Pubudu Saneth Samarakoon <saneth.samarakoon@gmail.com>
2023-10-20 11:09:49 +0200

    bwa-samtools bugfix

--- workflows/templates/bwa_mapping.sh
+++ workflows/templates/bwa_mapping.sh
@@ -8,7 +8,7 @@ FASTA=${REF}"/"${REF_MAP["reference_fasta"]["fna"]}
 bwa mem \
     -t ${task.cpus} \
     -K 10000000 \
-    -R "@RG\tID:${S_NAME}_rg1\tLB:lib1\tPL:bar\tSM:${S_NAME}\tPU:${S_NAME}_rg1" \
+    -R "@RG\\tID:${S_NAME}_rg1\\tLB:lib1\\tPL:bar\\tSM:${S_NAME}\\tPU:${S_NAME}_rg1" \
     \${FASTA} \
     ${R1} \
     ${R2} \
@@ -18,4 +18,4 @@ bwa mem \
     -m${task.memory.giga}g \
     -o ${S_NAME}_bwa-mem_${PROCESSOR}.bam -
 
-    samtools index -@4 -m4g ${S_NAME}_bwa-mem_${PROCESSOR}.bam
+samtools index -@4 -m4g -b ${S_NAME}_bwa-mem_${PROCESSOR}.bam
