03c5a0648759f2fa574ddf9421783e7316dd0f7b
Pubudu Saneth Samarakoon <saneth.samarakoon@gmail.com>
2023-10-26 11:54:13 +0200

    memory management

--- workflows/templates/gatk_apply_bqsr.sh
+++ workflows/templates/gatk_apply_bqsr.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 FASTA=${REF}"/"${REF_MAP["reference_fasta"]["fna"]}
 
 ## Run ApplyBQSR Step
-gatk --java-options -Xmx${task.memory.giga}g \
+gatk --java-options  "-Xmx${task.memory.giga}g -XX:-UsePerfData -Xms${task.memory.giga}g -Xss3m -XX:-UsePerfData" \
     ApplyBQSR \
     -R \${FASTA} \
     -I ${BAM} \
