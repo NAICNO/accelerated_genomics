fee7879d3b31089449eacfa7fbac44e30ee28842
Pubudu Saneth Samarakoon <saneth.samarakoon@gmail.com>
2023-12-20 12:14:08 +0100

    mosdepth update

--- workflows/templates/mosdepth.sh
+++ workflows/templates/mosdepth.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 FASTA=${REF}"/"${REF_MAP["reference_fasta"]["dict"]}
 
-awk '{if ((\$2 !~ /_/) && (\$1 !~ /HD/) ) {split(\$2,a,":"); split(a[2],b,"_"); split(\$3,c,":"); print b[1], c[2]}}' \${FASTA} > intervals.bed
+awk 'BEGIN{OFS="\\t"}{if ((\$2 !~ /_/) && (\$1 !~ /HD/) ) {split(\$2,a,":"); split(a[2],b,"_"); split(\$3,c,":"); print b[1], 1, c[2]}}' \${FASTA} > intervals.bed
 
 mosdepth \
 	-n \
